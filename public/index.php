1baa9ebd679e8de7562106b9917f1b7372e3aa9f
JesseOost64 <94468757+JesseOost64@users.noreply.github.com>
2022-03-31 21:15:11 +0200

    start login page

--- public/index.php
+++ public/index.php
@@ -6,9 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>GMR || Login</title>
-    <link rel="stylesheet" href="../assets/assets/styles/login.css">
+    <link rel="stylesheet" href="assets/styles/login.css">
 </head>
 <body>
+    <img src="assets/images/GMRTemp.png" alt="Temp GMR logo">
+    <h1>Login to your admin account</h1>
+    <div class="loginForm">
+        <form method="post" action="<?= htmlentities($_SERVER["PHP_SELF"]) ?>">
+            <input type="userName" name="userName" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
 
+            <input type="submit" value="login">
+        </form>
+    </div>
 </body>
 </html>
